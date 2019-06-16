30fd154e915a12d32cec46b200fa0b84f3642b30
xuansonvp94 <49930408+xuansonvp94@users.noreply.github.com>
2019-06-17 00:10:48 +0700

    calculator

---
+++ Caculator/script.js
@@ -0,0 +1,38 @@
+//vô hiệu hóa màn hình cal khi 
+$(document).ready(function() {
+	$('.calculation').attr('disabled', 'true'); 
+	$('.ipt').attr('disabled', 'true'); 
+});
+
+//các nút number
+$('button.number').on('click', function() {
+	let char = $(this).html(); // Lấy số vừa click
+	let value_calculation = $('.calculation').val(); // Lấy chuỗi giá trị trong ô calculation
+	$('.calculation').val(value_calculation + char); // Thêm số vừa click vào sau chuỗi giá trị trong ô calculation   
+});
+
+//dấu chấm
+
+$('.button-dot').on('click', function() {
+	let char = $(this).html(); 
+	let value_calculation = $('.calculation').val(); 
+	$('.calculation').val(value_calculation + char);
+})
+
+//nút AC
+$('.all-clear').on('click', function() {
+    $('.calculation').val(''); 
+    $('.ipt').val(''); 
+});
+
+//các nút phép tính
+ $('.pheptinh').on('click', function() {
+    $char = $(this).html(); 
+    $value_calculation = $('.calculation').val(); 
+    $('.calculation').val($value_calculation + $char);
+ });
+
+ $('.daubang').on('click', function() {
+    let result = $('.calculation').val(); 
+    $('.ipt').val(eval($result));
+});
