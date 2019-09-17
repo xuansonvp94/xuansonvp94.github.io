d0eca915943dfe7b7ae3a61fe7361be144c352ff
xuansonvp94 <49930408+xuansonvp94@users.noreply.github.com>
2019-09-18 02:21:56 +0700

    update responsive home

---
+++ time/js/home.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$("#single_item").slick({
+		dots: true,
+		centerMode: true,
+		centerPadding: '50px',
+		slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
+});
