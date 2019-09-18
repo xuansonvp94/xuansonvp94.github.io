8c6bcf41747e0bc8c8f27408c7cccde122bf5124
xuansonvp94 <49930408+xuansonvp94@users.noreply.github.com>
2019-09-19 01:55:34 +0700

    update product

--- time/js/product.js
+++ time/js/product.js
@@ -5,3 +5,13 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+function openNavRight() {
+  document.getElementById("mySidenav-right").style.width = "250px";
+}
+
+function closeNavRight() {
+  document.getElementById("mySidenav-right").style.width = "0";
+}
+
+
